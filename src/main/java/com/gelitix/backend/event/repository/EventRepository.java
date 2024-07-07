0b0acbe4fef6dad061a42cd181d4fa564ff07e4f
2code2code <hafidhaw@gmail.com>
2024-07-07 19:53:10 +0700

    fix : del unused function

--- src/main/java/com/gelitix/backend/event/repository/EventRepository.java
+++ src/main/java/com/gelitix/backend/event/repository/EventRepository.java
@@ -4,7 +4,6 @@ import com.gelitix.backend.event.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
